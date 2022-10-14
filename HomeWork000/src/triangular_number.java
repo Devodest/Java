b0b35838b6a5a4c0402729e85996a60b1eb8395e
Hart <Hartik1995@mail.ru>
2022-10-14 11:38:46 +0300

    add triangular_number.java - homeWork

--- HomeWork000/src/triangular_number.java
+++ HomeWork000/src/triangular_number.java
@@ -1,5 +1,14 @@
+//Написать программу вычисления n-ого треугольного числа.
+
+
 public class triangular_number {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int n = 5;
+        int result = 0;
+        for (int i = 1; i <= n; i++) {
+            result += i;
+            System.out.print(result + " ");
+
+        }
     }
 }
