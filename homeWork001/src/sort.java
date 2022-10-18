b9d1dcb6417dfe06d64dd5be9ad9842800fd88af
Hart <Hartik1995@mail.ru>
2022-10-18 10:15:54 +0300

    add homeWork sort.java

--- homeWork001/src/sort.java
+++ homeWork001/src/sort.java
@@ -1,19 +1,42 @@
+import java.util.Arrays;
 import java.util.Random;
 
 public class sort {
     public static void main(String[] args) {
         int[] randomArray = getRandomArray();
+        printArray(randomArray);
+        sortArray(randomArray);
+        printArray(randomArray);
     }
 
     private static int[] getRandomArray() {
-        Random rnd = new Random(10);
-        int[] array = new int[rnd.nextInt() + 1];
+        Random rnd = new Random();
+        int[] array = new int[10];
         for (int i = 0; i < array.length; i++) {
-            array[i] = rnd.nextInt();
+            array[i] = rnd.nextInt(10);
         }
         return array;
     }
 
-    private int []
+    private static void printArray(int[] array) {
+        System.out.println(Arrays.toString(array));
+    }
 
+    private static void sortArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int value = array[i];
+            int j = i - 1;
+            for (; j >= 0; j--) {
+                if (value < array[j]) {
+                    array[j + 1] = array[j];
+                } else {
+                    break;
+                }
+            }
+            array[j + 1] = value;
+        }
+    }
 }
+
+
+
